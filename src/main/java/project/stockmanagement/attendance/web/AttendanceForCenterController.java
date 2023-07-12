ab42ba1e5456e8bf5f2bc0a813881875bb580a5f
SUYEON KIM <waterkite94@gmail.com>
2023-07-12 21:31:59 +0900

    Fix: Change 출근 관리 컨트롤러 분리

--- src/main/java/project/stockmanagement/attendance/web/AttendanceForCenterController.java
+++ src/main/java/project/stockmanagement/attendance/web/AttendanceForCenterController.java
@@ -13,22 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import project.stockmanagement.attendance.service.AttendanceService;
 import project.stockmanagement.attendance.service.response.AttendanceResponse;
-import project.stockmanagement.attendance.web.request.AttendanceCreateRequest;
 import project.stockmanagement.attendance.web.request.AttendanceUpdateWorkStatusRequest;
 import project.stockmanagement.common.basewrapper.ApiResult;
 
 @RestController
 @RequiredArgsConstructor
-public class AttendanceController {
+public class AttendanceForCenterController {
 
 	private final AttendanceService attendanceService;
-
-	@PostMapping("/api/v1/attendance")
-	public ApiResult<AttendanceResponse> createAttendance(@Valid @RequestBody AttendanceCreateRequest request) {
-		AttendanceResponse attendanceResponse = attendanceService.createAttendance(request.toServiceRequest());
-
-		return ApiResult.onSuccess(attendanceResponse);
-	}
 
 	@GetMapping("/api/v1/attendance/employee/{id}")
 	public ApiResult<List<AttendanceResponse>> getEmployeeAttendances(@PathVariable Long id) {
